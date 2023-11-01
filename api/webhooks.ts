85aab7a9132874f7d64889c526c7dbaacd06e73e
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2023-11-02 00:23:06 +0330

    fix: update webhooks.ts export

--- api/webhooks.ts
+++ api/webhooks.ts
@@ -1,7 +1,7 @@
 import { createNodeMiddleware, createProbot } from "probot";
 import app from "../src/app";
 
-module.exports = createNodeMiddleware(app, {
+export default createNodeMiddleware(app, {
   probot: createProbot(),
   webhooksPath: "api/webhooks",
 });
